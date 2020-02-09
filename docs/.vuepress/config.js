6349fd36004fb7fbe41f7db778857bf6c19d28a1
Wisse <wissehes@gmail.com>
2020-02-09 22:07:26 +0100

    added links and fixed head thingy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,13 +4,14 @@ const barConfig = getConfig(`${__dirname}/..`)
 module.exports = {
     title: 'Skeppy Bot',
     description: 'The Discord bot for Skeppy fans!',
+    head: [
+        ['link', { type: "image/png", sizes: "256x256", rel: 'icon', href: '/images/favicon.png' }]
+        ['link', { rel: "shortcut icon", type: "image/png", href: "/images/favicon.png" }]
+    ],
     themeConfig: {
-        head: [
-            ['link', { type: "image/png", sizes: "256x256", rel: 'icon', href: '/images/favicon.png' }]
-            ['link', { rel: "shortcut icon", type: "image/png", href: "/images/favicon.png" }]
-        ],
         nav: [
-            { text: 'invite', link: 'https://discordapp.com/oauth2/authorize?client_id=579327336395309062&scope=bot&permissions=2084044031', ...barConfig.nav}
+            { text: 'Invite', link: 'https://discordapp.com/oauth2/authorize?client_id=579327336395309062&scope=bot&permissions=2084044031' },
+            { text: 'Vote', link: 'https://top.gg/bot/579327336395309062' }
         ],
         sidebar: barConfig.sidebar,
     }
